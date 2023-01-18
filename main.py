3700bcd360337504d060c47e14ee70a474b133cc
Bartosz Malec <bartosz.malec@onet.pl>
2023-01-18 22:11:46 +0100

    Added comments

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from controller import TextController
 root = tkinter.Tk()  # Initialize tkinter
 app = TextController(root)  # Create an instance of the TextController
 root.mainloop()  # Run graphical interface
+
+
+
+# Szkielet aplikacji oraz model (model.py) - Krzysztof Kwiatkowski
+# Testy jednostkowe wraz z podsumowaniem czasu wykonania - Krzysztof Kwiatkowski
+# Interfejs graficzny - Bartosz Malec
+# Algorytm Boyer'a i Moore'a - Wspólna implementacja
